112676199277f1156cd4cd56e7f929d7b74f7494
Emiko <emiko.yonekura@gmail.com>
2016-06-10 20:41:52 -0300

    pregunta 2 ok

--- pregunta2/main.js
+++ pregunta2/main.js
@@ -1,1 +1,15 @@
-main.js
+var suma = 0;
+
+do {
+
+	var valor = prompt("ingresa un número");
+	if (!isNaN(valor)){
+
+	suma = suma + parseInt(valor);
+	}
+}
+
+while (valor != "fin");
+	
+
+console.log(suma)
